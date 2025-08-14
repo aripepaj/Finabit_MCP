bd2ceedcaa097ef47721d9f60586c09d6ecbe9e4
aripepaj <aripepaj03@gmail.com>
2025-08-14 15:50:38 +0200

    Bug fixing

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",   
+        extra="ignore",   
+    )
     faq_api_url: str
     server_api_url: str
     debug: bool = False
